02c6517ca13358f2bb11adfc39cb0f5be21ed14e
Jarren San Jose <sanjose.jarren@gmail.com>
2024-04-25 17:25:14 -0700

    reorder functions

--- contracts/HonoraryWeaxels.sol
+++ contracts/HonoraryWeaxels.sol
@@ -13,24 +13,23 @@ contract HonoraryWeaxels is Ownable, ERC721A {
 
     constructor() ERC721A("HonoraryWeaxels", "HWXL") {}
 
-    function mint(uint256 quantity, address receiver) public onlyOwner {
-        _safeMint(receiver, quantity);
-    } 
-
-    function setBaseURI(string memory _baseURI) public onlyOwner {
-        BASE_URI = _baseURI;
-    }
-
     function contractURI() public view returns (string memory) {
         return CONTRACT_URI;
+    }
+
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        return string(abi.encodePacked(BASE_URI, "/", Strings.toString(_tokenId)));
+    }
+
+    function mint(uint256 quantity, address receiver) public onlyOwner {
+        _safeMint(receiver, quantity);
     }
 
     function setContractURI(string memory _contractURI) public onlyOwner {
         CONTRACT_URI = _contractURI;
     }
 
-    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
-        return string(abi.encodePacked(BASE_URI, "/", Strings.toString(_tokenId)));
+    function setBaseURI(string memory _baseURI) public onlyOwner {
+        BASE_URI = _baseURI;
     }
 }
-
